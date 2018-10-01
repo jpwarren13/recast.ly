9ebb138157ef7f41d4e4960337eb27eea83932ce
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-10-01 15:04:20 -0700

    Rendered initial App view

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+// import Search from './components/Search.js';
+// import VideoList from './components/VideoList.js';
+// import VideoListEntry from './components/VideoListEntry.js';
+// import VideoPlayer from './components/VideoPlayer.js';
 
+ReactDOM.render(<App />, document.getElementById('app'));
